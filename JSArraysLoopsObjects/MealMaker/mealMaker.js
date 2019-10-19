5f33ceb8ab51a842df8af6b549515c1fb0a9e9e2
Matt Ellisor <ellisor.matt@gmail.com>
2019-10-19 17:06:01 -0500

    completed lab

--- JSArraysLoopsObjects/MealMaker/mealMaker.js
+++ JSArraysLoopsObjects/MealMaker/mealMaker.js
@@ -28,6 +28,44 @@ const menu = {
             mains: this.mains,
             desserts: this.desserts
         }
+    },
+    addDishToCourse (courseName,dishName,dishPrice) {
+        let dish = {
+            name: dishName,
+            price: dishPrice
+        };
+        this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse (courseName) {
+        //get a random dish from a specified course
+        let dishes = this._courses[courseName];
+        selectDish = Math.floor(Math.random()*dishes.length);
+        return dishes[selectDish];
+    },
+    generateRandomMeal () {
+        //automatically create a three course meal
+        let generatedMeal = {
+            appetizer: this.getRandomDishFromCourse('appetizers'),
+            main: this.getRandomDishFromCourse('mains'),
+            dessert: this.getRandomDishFromCourse('desserts')
+        }
+        return generatedMeal;
     }
     
 };
+
+
+menu.addDishToCourse('appetizers','testApp1','$5');
+menu.addDishToCourse('mains','testMain1','$6');
+menu.addDishToCourse('desserts','testDessert1','$7');
+
+menu.addDishToCourse('appetizers','testApp2','$5');
+menu.addDishToCourse('mains','testMain2','$6');
+menu.addDishToCourse('desserts','testDessert2','$7');
+
+menu.addDishToCourse('appetizers','testApp3','$5');
+menu.addDishToCourse('mains','testMain3','$6');
+menu.addDishToCourse('desserts','testDessert3','$7');
+
+console.log(menu.getRandomDishFromCourse('appetizers'));
+console.log(menu.generateRandomMeal());
